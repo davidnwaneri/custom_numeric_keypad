50c880bb5e3b976e40d08e32bd719f2825d78fbe
David Nwaneri <davidn4333@gmail.com>
2023-03-14 07:57:48 +0100

    testing gpg key

--- final/lib/numeric_keypad.dart
+++ final/lib/numeric_keypad.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 /// A widget that builds a numeric keypad which
 /// mimics the Os numeric keypad on Android and IOS devices
-/// or other touch-enabled devices.
+/// or other touch-enabled devices
 class NumericKeyPad extends StatelessWidget {
   /// Creates a widget that builds the numeric keypad.
   const NumericKeyPad({
